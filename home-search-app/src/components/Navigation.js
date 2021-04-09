d5ab770a108242487c12c781cee3b39d50ea4dd1
Aishwarya Yatin Samel <samel.a@northeastern.edu>
2021-04-09 11:10:52 -0400

    app structure

---
+++ home-search-app/src/components/Navigation.js
@@ -0,0 +1,14 @@
+import './Navigation.scss';
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+
+function Navbar(){
+
+return(
+    <>
+    </>
+);
+
+}
+
+export default Navbar;
