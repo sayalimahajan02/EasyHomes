e1e661440ea539149cdf98f3795638d29aac308b
prasadAjay <prasad.aj@northeastern.edu>
2021-04-12 18:29:16 -0400

    Added Home Page

---
+++ home-search-app/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './../components/Header.scss';
+import Logo from './images/homeLogo.PNG';
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='header-wrapper'>
+        <header>
+          <nav className='left-nav'>
+            <a href='/property'>Buy</a>
+            <a href='/property'>Sell</a>
+          </nav>
+          <div className='logo'>
+            <a href='/'><img src={Logo} className="homeLogo" />
+              <span>Easy Homes</span></a>
+          </div>
+          <nav className='right-nav'>
+            <a href='/property'>SignIn or SignUp</a>
+            <a href='/property'>Help</a>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Header;
