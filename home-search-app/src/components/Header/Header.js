b54e9bee7ca29c9fd450cb3d9e407cdc80b6ffce
prasadAjay <prasad.aj@northeastern.edu>
2021-04-14 12:48:06 -0400

    Added Nav Links for Signup and Login Page

--- home-search-app/src/components/Header/Header.js
+++ home-search-app/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './../components/Header.scss';
-import Logo from './images/homeLogo.PNG';
+import { NavLink } from 'react-router-dom';
+import './../Header/Header.scss';
+import Logo from './../images/homeLogo.PNG';
 
 class Header extends React.Component {
 
@@ -13,17 +14,18 @@ class Header extends React.Component {
       <div className='header-wrapper'>
         <header>
           <nav className='left-nav'>
-            <a href='/property'>Buy</a>
-            <a href='/property'>Sell</a>
+            <NavLink to='/aboutus'>About Us</NavLink>
+            <NavLink to='/property'>Buy</NavLink>
+            <NavLink to='/property'>Sell</NavLink>
           </nav>
           <div className='logo'>
             <a href='/'><img src={Logo} className="homeLogo" />
               <span>Easy Homes</span></a>
           </div>
           <nav className='right-nav'>
-            <a href='/login'>SignIn</a>
-            <a href='/signup'>SignUp</a>
-            <a href='/property'>Help</a>
+            <NavLink to='/login'>SignIn</NavLink>
+            <NavLink to='/signup'>SignUp</NavLink>
+            <NavLink to='/faq'>FAQs</NavLink>
           </nav>
         </header>
       </div>
--- home-search-app/src/components/Signup/Signup.js
+++ home-search-app/src/components/Signup/Signup.js
@@ -4,119 +4,122 @@ import Input from "react-validation/build/input";
 import signupService from '../../services/user.service';
 import { Redirect } from 'react-router-dom';
 import GoogleLogin from 'react-google-login';
+import Header from './../Header/Header';
+
 export default class Register extends React.Component {
   constructor(props) {
     super(props);
-    this.RegisterMethod=this.RegisterMethod.bind(this);
-    this.onChangeOfEmail=this.onChangeOfEmail.bind(this);
-    this.onChangeOfPassword=this.onChangeOfPassword.bind(this);
-    this.responseErrorGoogle=this.responseErrorGoogle.bind(this);
-    this.responseSuccessGoogle=this.responseSuccessGoogle.bind(this);
+    this.RegisterMethod = this.RegisterMethod.bind(this);
+    this.onChangeOfEmail = this.onChangeOfEmail.bind(this);
+    this.onChangeOfPassword = this.onChangeOfPassword.bind(this);
+    this.responseErrorGoogle = this.responseErrorGoogle.bind(this);
+    this.responseSuccessGoogle = this.responseSuccessGoogle.bind(this);
     // state for signup component
     this.state = {
       email: "",
       password: "",
-      users :[],
-      displayMessage :""
+      users: [],
+      displayMessage: ""
     };
   }
- 
-  onChangeOfEmail(e){
-    this.setState({email:e.target.value});
-  }
-  onChangeOfPassword(e){
-    this.setState({password:e.target.value});
-  }
 
-  RegisterMethod(e){
-e.preventDefault();
-for(let i=0;i<this.state.users.length;i++) {
-  if(this.state.users[i].emailId==this.state.email){
-  this.setState({displayMessage:'User with this email address already registered'});
-  return;
-}
-}
-this.setState({displayMessage:''});
-signupService.register(this.state.email,this.state.password).then(response => response.data.emailId);
-return <Redirect to ="/"/>
+  onChangeOfEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+  onChangeOfPassword(e) {
+    this.setState({ password: e.target.value });
   }
 
-//google starts
- responseSuccessGoogle = (response) => {
-  console.log(response);
-  console.log(response.profileObj);
-  for(let i=0;i<this.state.users.length;i++) {
-		if(this.state.users[i].emailId==response.profileObj.email){
-    this.setState({displayMessage:'User with this email address already registered. Please login'});
-    return;
-  }
-}
-  this.setState({displayMessage:''});
-  signupService.register(response.profileObj.email,response.tc.login_hint).then(response => response.data.emailId);
-
-}
- responseErrorGoogle = (response) => {
-  console.log(response);
-}
-
-componentWillMount(){
-	this.getData()
-}
-getData(){
-	const tmpArray = [];
-	fetch('http://localhost:3000/records/')
-	 .then(response => response.json())
-	.then(response => {
-		for (var i = 0; i < response.length; i++) {
-			tmpArray.push(response[i])
-		}
-	
-		this.setState({
-			users: tmpArray
-		})
-
-	})
-	.catch(error => this.setState({ error }));
+  RegisterMethod(e) {
+    e.preventDefault();
+    for (let i = 0; i < this.state.users.length; i++) {
+      if (this.state.users[i].emailId == this.state.email) {
+        this.setState({ displayMessage: 'User with this email address already registered' });
+        return;
+      }
+    }
+    this.setState({ displayMessage: '' });
+    signupService.register(this.state.email, this.state.password).then(response => response.data.emailId);
+    return <Redirect to="/" />
   }
 
-//google ends
+  //google starts
+  responseSuccessGoogle = (response) => {
+    console.log(response);
+    console.log(response.profileObj);
+    for (let i = 0; i < this.state.users.length; i++) {
+      if (this.state.users[i].emailId == response.profileObj.email) {
+        this.setState({ displayMessage: 'User with this email address already registered. Please login' });
+        return;
+      }
+    }
+    this.setState({ displayMessage: '' });
+    signupService.register(response.profileObj.email, response.tc.login_hint).then(response => response.data.emailId);
+
+  }
+  responseErrorGoogle = (response) => {
+    console.log(response);
+  }
+
+  componentWillMount() {
+    this.getData()
+  }
+  getData() {
+    const tmpArray = [];
+    fetch('http://localhost:3000/records/')
+      .then(response => response.json())
+      .then(response => {
+        for (var i = 0; i < response.length; i++) {
+          tmpArray.push(response[i])
+        }
+
+        this.setState({
+          users: tmpArray
+        })
+
+      })
+      .catch(error => this.setState({ error }));
+  }
+
+  //google ends
   render() {
     return (
       <div>
-          <Form onSubmit={this.RegisterMethod}>
-          
-                   <label htmlFor="email">Email</label>
-                  <Input
-                    type="text"
-                    className="form-control"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.onChangeOfEmail}
-                   
-                    /> {this.state.displayMessage}
-<br></br>
-                   <label htmlFor="password">Password</label>
-                  <Input
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.onChangeOfPassword}
-                  />
-                  <button>Signup</button>
-          </Form>
+        <Header />
+        <Form onSubmit={this.RegisterMethod}>
+
+          <label htmlFor="email">Email</label>
+          <Input
+            type="text"
+            className="form-control"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChangeOfEmail}
+
+          /> {this.state.displayMessage}
+          <br></br>
+          <label htmlFor="password">Password</label>
+          <Input
+            type="password"
+            className="form-control"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChangeOfPassword}
+          />
+          <button>Signup</button>
+        </Form>
 
         ----------------OR-------------------
         <br></br>
-<GoogleLogin
-clientId="402608281823-mfler2nvm70fq6jab80330m767f7rtde.apps.googleusercontent.com"
-buttonText="SignUp with Google"
-onSuccess={this.responseSuccessGoogle}
-onFailure={this.responseErrorGoogle}
-cookiePolicy={'single_host_origin'}
-/>
+        <GoogleLogin
+          clientId="402608281823-mfler2nvm70fq6jab80330m767f7rtde.apps.googleusercontent.com"
+          buttonText="SignUp with Google"
+          onSuccess={this.responseSuccessGoogle}
+          onFailure={this.responseErrorGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
 
-     </div>
+      </div>
     );
   }
 }
