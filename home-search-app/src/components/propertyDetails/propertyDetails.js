18c328662d334986a1c2245edcee26b177bce0de
Aishwarya Yatin Samel <samel.a@northeastern.edu>
2021-04-17 23:49:06 -0400

    added property details

--- home-search-app/src/components/propertyDetails/propertyDetails.js
+++ home-search-app/src/components/propertyDetails/propertyDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './propertyDetails.scss';
-import img1 from './images/property.jpeg';
+import img1 from '../images/property.jpeg';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
 
@@ -15,78 +15,163 @@ class propertyDetails extends React.Component{
             "propertySqftArea":"",
             "propertyPrice":0,
             "propertyStreet":"",
+            "bed":0,
+            "bath":0,
             "propertyCity":"",
             "propertyState":"",
             "propertyZipcode":0,
             "propertyBuildDate":"",
             "seller":[],
             "buyer":[],
-            "data":[]
+            "data":[],
+            disableBtn:false
           }
-        //   this.submitForm = this.submitForm.bind(this);
-          this.handleChange = this.handleChange.bind(this);
-        //   this.checkBox = this.checkBox.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.addToBuyer = this.addToBuyer.bind(this);
     }
 
  
-
     //to set state
     handleChange=(event,field)=>{ 
         this.setState({[field]:event.target.value})
       }
 
-    // Form Submit Function
-    // submitForm(e){
-    //     e.preventDefault();
-    //     this.getData();
-    //     console.log(this.state)
-    
-    //     console.log("Form Saved");
-    // }
-
-
-
+    //when page loads
     componentWillMount(){
       this.getData()
+      // this.checkProp()
     }
+
+    // checkProp(){
+    //   console.log("heyy "+ this.state.disableBtn)
+    //   if(!this.state.data.buyer){
+    //     this.setState({
+    //       disableBtn: false
+    //     })
+    //   }
+    //   else{
+    //     this.setState({
+    //       disableBtn: true
+    //     })
+    //   }
+    // }
+
+    addToBuyer(){
+      
+      if(!this.state.data.buyer){
+        const temp = this.state.data;
+        temp.buyer = localStorage.getItem('user');
+        this.setState({
+          data: temp,
+          disableBtn: true
+        })
+        // document.getElementsByClassName("offer-btn-class").value = "Property Sold"
+        this.putData(this.state.data,this.state.disableBtn)
+      }
+      else{
+        alert("this property already has a buyer")
+      }
+    }
+
+
+        //add new task to list
+        addNewTask = () => {
+          this.setState({
+              showDiv: !this.state.showDiv
+          })
+        }
+
     // Get Data 
     getData(){
       const tmpArray =[];
-      fetch('http://localhost:3000/homeSearch/6071d5af87b776a16c935681')
+      fetch('http://localhost:3000/homeSearch/607b7793644d971645f3f233')
       .then(response => response.json())
      .then(responseList => {
                 this.setState({
                     data: responseList
                 })
-              console.log("hey  "+ this.state.data.propertyDesc)
+              // console.log("hey  "+ this.state.data.propertyDesc)
             })
             .catch(error => {
                 console.log("error:" + error)
             });
           }
 
+    //put data
+    putData(data,disableBtn) {
+      fetch('http://localhost:3000/homeSearch/607b7793644d971645f3f233', {
+          method: 'PUT',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              buyer: this.state.data.buyer,
+              disableBtn: this.state.data.disableBtn
+          })
+      }).then(response => {
+          console.log("response:" + response.body)
+      })
+          .catch(error => {
+              console.log("error:" + error)
+          })
+
+  }
+
 
     render(){
         return(
           
           <> 
-          {/* <div class = "item-grid"> */}
-            <div>
-                <AliceCarousel autoPlay autoPlayInterval="3000">
-                    <img src={img1} className="sliderimg"/>
-                    <img src={img1} className="sliderimg"/>
-                    <img src={img1} className="sliderimg"/>
-                    <img src={img1} className="sliderimg"/>
-
-                </AliceCarousel>
-              </div> 
-              
-              <div class = "prop-det">{this.state.data.propertyDesc}</div>
-          {/* </div> */}
-            
+          <div class="row">
+                <div className="column1">
+                    <AliceCarousel autoPlay autoPlayInterval="3000">
+                        <img src={img1} className="sliderimg"/>
+                        <img src={img1} className="sliderimg"/>
+                        <img src={img1} className="sliderimg"/>
+                    </AliceCarousel>  
+                      <div class="prop-add">{this.state.data.propertyStreet}, {this.state.data.propertyCity}, {this.state.data.propertyState} - {this.state.data.propertyZipcode}</div>
+                      <div class="prop-price">${this.state.data.propertyPrice}</div>
+                </div>
+                <div class ="column2">
+                  <div>{this.state.data.propertyName}</div>
+                  <div className = "todo-expand">{this.state.data.propertyDesc}</div>
+                    
+                    <table>
+                    <tr>
+                      <th>{this.state.data.bed}</th>
+                      <th>{this.state.data.bath}</th>
+                      <th>{this.state.data.propertySqftArea}</th>
+                    </tr>
+                    <tr>
+                      <td>BED</td>
+                      <td>BATH</td>
+                      <td>Sqft</td>
+                    </tr>
+                  </table>
+                  <div className = "todo-expand">
+                    <label>Property Type</label>  
+                    <input id = "proptype" value = {this.state.data.propertyType} disabled></input>
+                    <label>Property Build Date</label>  
+                    <input id = "propdate" value = {this.state.data.propertyBuildDate} disabled></input>
+                  </div>
+                    <button className ="contact-seller-btn" onClick={this.addNewTask}>Contact Seller</button>
+                    
+                      {this.state.showDiv ? <div>
+                          <div className="seller-details">
+                            <label>Email</label>  
+                            <input id = "email" value = {this.state.data.seller.emailId} disabled></input>
+                            <label>Contact</label>  
+                            <input id = "contact" value = {this.state.desc} disabled></input>
+                          </div>
+                    </div> : null}
+                  <div className = "offer-btn-class"><button disabled={this.state.disableBtn} className="offer-btn" onClick ={this.addToBuyer}>Start an Offer</button></div>
+              </div>
+          </div>
           </>
         )
     }
 }
 
 export default propertyDetails;
+
